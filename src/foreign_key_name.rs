c4352dbcd62f66cc12f5ab8378825ec9ac40559d
Tokarev Artem <tokarev28.art@gmail.com>
2023-04-08 01:32:46 +0500

    update foreign key name implementation

--- src/foreign_key_name.rs
+++ src/foreign_key_name.rs
@@ -1,11 +1,15 @@
-use crate::table::{TableColumn, Unquote};
+use crate::table::{Table, TableColumn, Unquote};
 
 pub trait ForeignKeyName: TableColumn {
     fn foreign_key_name<F: TableColumn>(&self, fcol: F) -> String {
         let q = if !Self::QUOTE.is_empty() {
             Self::QUOTE
-        } else {
+        } else if !Self::Table::QUOTE.is_empty() {
+            Self::Table::QUOTE
+        } else if !F::QUOTE.is_empty() {
             F::QUOTE
+        } else {
+            F::Table::QUOTE
         };
         format!(
             "{q}fk_{}_{}_{}_{}{q}",
