f7ae408acee90d0638e42008fe3b77bbcda8d8d3
Tokarev Artem <tokarev28.art@gmail.com>
2023-07-30 18:12:08 +0500

    Add more reexports

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,10 @@ pub mod index_name;
 pub mod qualified;
 pub mod table;
 
+pub use foreign_key_name::ForeignKeyName;
+pub use index_name::IndexName;
+pub use qualified::Qualified;
+pub use table::{Table, TableColumn, Unquote};
+
 pub use paste;
 pub use sql_table_inject::inject;
